78f74e0db3061821c1c03e803a0698d5f8fc2f92
kpllahore123-maker <kpllahore123@gmail.com>
2026-06-26 01:04:47 +0500

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDOBynDQ00o2Yh_TD9rsQnHypf97ne6hmM",
+  authDomain: "arenax-c1586.firebaseapp.com",
+  projectId: "arenax-c1586",
+  storageBucket: "arenax-c1586.firebasestorage.app",
+  messagingSenderId: "1069776825982",
+  appId: "1:1069776825982:web:f2d7f11cef4c206206b22f",
+  measurementId: "G-G95P4JY3J1"
+};
