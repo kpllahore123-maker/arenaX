df4a004e98ac74d7bc8904cc79654710465aae96
kpllahore123-maker <kpllahore123@gmail.com>
2026-09-21 22:50:43 +0500

    Override onCreate to register NativeSettingsPlugin

--- android/app/src/main/java/com/arenax/esports/MainActivity.java
+++ android/app/src/main/java/com/arenax/esports/MainActivity.java
@@ -1,5 +1,12 @@
 package com.arenax.esports;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(NativeSettingsPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
